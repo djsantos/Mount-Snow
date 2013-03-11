1160d49175226316dd0df060c4bbe15ad73784ab
Daniel Santos <djsantos@student.umass.edu>
2013-03-11 17:23:04 -0400

    functional welcome page library

--- routes/welcome.js
+++ routes/welcome.js
@@ -9,9 +9,7 @@ exports.login = function (req, res) {
 	var name = req.body.username;
 	var password = req.body.password;
 	if(wellib.login(name,password))
-	//performs function of intended library call until library call is fixed
-	//if(name&&password)
-    	//res.redirect('/home'); 
+    	res.redirect('/home'); 
     else res.redirect('/welcome');
 };
 
