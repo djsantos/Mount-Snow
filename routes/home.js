45049a3063dba7d2138f9ade8e9ee6fd2095688b
bmentzer <bmentzer@student.umass.edu>
2013-03-11 23:05:37 -0400

    comments

--- routes/home.js
+++ routes/home.js
@@ -1,3 +1,11 @@
+/*
+@module routes/home
+*/
+
+
+/*
+*requires lib/home
+*/
 var homeLib = require('../lib/home');
 var tweetFeed = new Array();
 var myUsername = null;
@@ -5,6 +13,10 @@ var myTweets = 0;
 var myFollowing = 0;
 var myFollowers = 0;
 
+/*
+*When home is loaded we call the retrieveVariables function
+*to assign the various variables. Then render the page.
+*/
 exports.home = function(req,res){
 	retrieveVariables();
 	res.render('home', {
@@ -16,6 +28,10 @@ exports.home = function(req,res){
 	feed:tweetFeed
 	});
 }
+
+/*
+*Calls the database to get the needed values.
+*/
 function retrieveVariables(){
 	myTweets = homeLib.numTweets();
 	myFollowing = homeLib.numFollowing();
--- routes/search.js
+++ routes/search.js
@@ -1,6 +1,17 @@
+/*
+*@module routes/search
+*/
+
+/*
+*requires lib/search
+*/
 var searchLib = require('../lib/search');
 var searchResults = new Array();
 
+/*
+*When search is loaded we call the retrieveResults function
+*to assign the searchResults variable. Then render the page.
+*/
 exports.search = function(req,res){
 	retrieveResults(req);
 	res.render('search', {
@@ -8,9 +19,14 @@ exports.search = function(req,res){
 	searchContent: searchResults
 	});
 }
+
+/*
+*Calls the database to get the results if a query was passed.
+*/
 function retrieveResults(req){
 	var requestParam = req.param('q');
-	if(requestParam == null) searchResults[0] = 'Please submit a search.';
+	if(requestParam == '') searchResults[0] = 'Please submit a search.';
+	else if(requestParam == null) searchResults[0] = 'Please submit a search.';
 	else searchResults = searchLib.findResults(requestParam);
 	console.log('search query = ' + requestParam);
 }
