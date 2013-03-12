598df963ce4d28da1ae8b64bd9826637404bf5ad
ddebened <ddebened@student.umass.edu>
2013-03-12 00:10:40 -0300

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -1,6 +1,13 @@
+/*
+@module routes/activity
+*/
+
+//renders activity view 
+
 exports.activity = function(req,res){
   res.render('activity', {title: 'Twitter'});
 }
+
 
 exports.discover = function(req,res){
 	res.redirect('/discover');
