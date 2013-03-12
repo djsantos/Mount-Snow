8489deaf6dfc8d070b12aeeab817458a2df58d20
mbohling <mbohling@student.umass.edu>
2013-03-12 00:08:43 -0300

    Update signup.js

--- routes/signup.js
+++ routes/signup.js
@@ -1,10 +1,24 @@
+/*
+@module routes/signup
+*/
+
+/*
+*requires lib/signup
+*/
+
 var signupLib = require('../lib/signup');
 
-
+/*
+*renders the signup view
+*/
 exports.signup = function(req,res){
 	res.render('signup', {title: 'Twitter'});
 }
 
+/*
+*creates variables for each parameter in the form for signing up
+*and creates the account with the input information
+*/
 exports.CreateAccount = function (req, res) {
 	var name = req.body.name;
 	var email = req.body.email;
