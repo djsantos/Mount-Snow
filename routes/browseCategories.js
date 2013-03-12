296d812197d1c87324441a44b2724ba56bc33db3
ddebened <ddebened@student.umass.edu>
2013-03-11 21:51:17 -0300

    Create browseCategories.js

---
+++ routes/browseCategories.js
@@ -0,0 +1,15 @@
+exports.browseCategories = function(req,res){
+  res.render('browseCategories', {title: 'Twitter'});
+}
+
+exports.discover = function(req,res){
+	res.redirect('/discover');
+};
+
+exports.activity = function(req,res){
+	res.redirect('/activity');
+};
+
+exports.findFriends = function(req,res){
+	res.redirect('/findFrineds');
+};
