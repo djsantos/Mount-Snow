b8b5558d6d3c3f8338d4da2d4f0cdc8a89d95bfc
ddebened <ddebened@student.umass.edu>
2013-04-02 20:20:55 -0300

    began to add ability to tweet from display

--- routes/me.js
+++ routes/me.js
@@ -14,4 +14,10 @@ exports.me = function(req,res){
 	var results = melib.tweets();
 	res.render('me', {title: 'Twitter',tweets:results});
 }
+
+//adds ability to tweet from the display
+exports.tweet = function (req, res) {
+	var tweet = req.body.tweet;
+	melib.createTweet(tweet);
+};
  
