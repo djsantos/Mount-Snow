557687629b426b06aad60afbe598ef84364a9912
ddebened <ddebened@student.umass.edu>
2013-04-04 00:47:26 -0300

    Update following.js

--- routes/following.js
+++ routes/following.js
@@ -2,13 +2,21 @@
 @module routes/following
 */
 
+/*
+* requires /lib/follow
+*/
+var followlib = require('../lib/follow');
+
 var myUsername = null;
 
 //renders following view 
 exports.following = function(req,res){
   myUsername = req.session.user;
+  myUID = myUsername.uid;
+  followingList = followLib.displayFollowing(myUID);
   res.render('following', {
 	title: 'Twitter',
 	username: myUsername
+	following: followingList
   });
-}
+};
