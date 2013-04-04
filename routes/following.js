6b227ea43479ca1c2c768142d9dfbc711f3cb659
ddebened <ddebened@student.umass.edu>
2013-04-04 14:33:53 -0300

    Update following.js

--- routes/following.js
+++ routes/following.js
@@ -13,7 +13,7 @@ var myUsername = null;
 exports.following = function(req,res){
   myUsername = req.session.user;
   myUID = myUsername.uid;
-  followingList = followLib.displayFollowing(myUID);
+  var followingList = followLib.displayFollowing(myUID);
   res.render('following', {
 	title: 'Twitter',
 	username: myUsername,
