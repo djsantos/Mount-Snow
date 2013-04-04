1c7dc01585062a724ef40c9ca01b05da37362378
ddebened <ddebened@student.umass.edu>
2013-04-04 15:26:17 -0300

    Update connect.js

--- routes/connect.js
+++ routes/connect.js
@@ -11,7 +11,7 @@ var connectLib = require('../lib/connect');
 //renders connect view
 exports.connect = function(req,res){
   myUsername = req.session.user;
-  var myUID = myUsername.uid;
+  var myUID = req.session.uid;
   
   var connectList = connectLib.displayConnect(myUID);
   res.render('connect', {
