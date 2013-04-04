fa2f16f4185986926e36bbfb612dc40863e30c59
bmentzer <bmentzer@student.umass.edu>
2013-04-03 21:19:21 -0400

    compose button

--- routes/compose.js
+++ routes/compose.js
@@ -1,25 +1,33 @@
 // library dependencies
 var Tweetlib = require('../lib/tweet');
+var message = "Tweets can be up to 180 characters.";
 
 //renders the compose tweet page
 
 exports.compose = function(req,res){
 	console.log('composing');
-	res.render('compose', {title: 'Twitter'});
+	res.render('compose', {
+		title: 'Twitter',
+		message: message
+	});
 };
 exports.tweet = function(req,res){
 	var tweet = req.body.tweet;
-	var uid = req.session.user.userid;
+	var uid = req.session.user.uid;
+	console.log(uid);
 	//library call for adding tweet to database object added here
-	Tweetlib.createTweet(tweet, uid, function(error, u){
+	Tweetlib.createTweet(tweet, 1, function(error, uid){
 		if(error == 'too long'){
-			//display a message
+			message = "Your tweet was too long. Please try again.";
+			res.redirect('/compose');
 		}
 		else if(error == 'invalid tweet'){
-			//display a message
+			message = "That was an invalid tweet. Make sure you are logged in and please try again.";
+			res.redirect('/compose');
 		}
 		else{
 			//returns user home if the tweet was valid
+			message = "Tweets can be up to 180 characters.";
 			res.redirect ('/home');
 		}
 });
