33a9384f55489a255a963d0f2a5c0268560d7bdb
ddebened <ddebened@student.umass.edu>
2013-04-18 19:43:10 -0300

    Update mentions.js

--- routes/mentions.js
+++ routes/mentions.js
@@ -1,15 +1,26 @@
 /*
 @module routes/mentions
 */
+
+/*
+ * GET connect file
+ */
+var connectLib = require('../lib/connect');
+
 var myUsername = null;
+var uid = null;
 
 //renders mentions view
 
 exports.mentions = function(req,res){
   myUsername = req.session.user;
+  myUID = req.session.uid;
+  
+  var mentionList = connectLib.displayMentions(uid);
   res.render('mentions', {
 	title: 'Twitter',
 	username: myUsername
+	mentions: mentionList
   });
 }
 
