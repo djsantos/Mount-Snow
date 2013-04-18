69e3743154f3bb193892d3b835764712c301ea2f
ddebened <ddebened@student.umass.edu>
2013-04-18 02:09:59 -0300

    Update compose.js

--- routes/compose.js
+++ routes/compose.js
@@ -32,7 +32,7 @@ exports.post = function (req, res) {
 	var uid = req.session.uid;
 	
 	if(name.indexOf("@") !== -1){
-		var userName = name.substring(name.indexOf("@", 0));
+		var userName = name.substring(name.indexOf("@"));
 		console.log(userName);
 		uid = user.getUid(userName);
 		console.log(uid);
