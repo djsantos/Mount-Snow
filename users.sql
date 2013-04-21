e17357f6f017f2d3b24ece8b63bf0d94fe768cdc
mbohling <mbohling@student.umass.edu>
2013-04-21 20:47:30 -0300

    Update users.sql

--- users.sql
+++ users.sql
@@ -1,6 +1,16 @@
 create table users (
- uid integer primary key autoincrement,
+ 	uid integer primary key autoincrement,
 	username varchar(255) not null,
 	password varchar(255) not null,
 	email varchar(255) not null,
+);
+
+create table tweets (
+	uid integer,
+	message username varchar(255) not null,
+);
+
+create table follow (
+	uid integer,
+	theirID integer,
 );
