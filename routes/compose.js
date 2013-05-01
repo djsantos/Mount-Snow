6043d08af8818dccf3f36f1e00240e90198c01c0
ddebened <ddebened@student.umass.edu>
2013-05-01 18:21:56 -0300

    Update compose.js

--- routes/compose.js
+++ routes/compose.js
@@ -76,10 +76,22 @@ exports.post = function (req, res) {
 // expected to send a post request with a JSON body containing
 // a single object: { last : <value> }.
 exports.check = function (req, res) {
-	var last = parseInt(req.body.last, 10);
-	var rest = posts.slice(last, posts.length);
-	//myUID = req.session.uid;
-	//var rest = Tweetlib.displayTweets(myUID);
+	//var last = parseInt(req.body.last, 10);
+	//var rest = posts.slice(last, posts.length);
+	myUID = req.session.uid;
+	Tweetlib.displayTweets(myUID, function(error, tweets){
+			if (error){
+				console.log('Error: ' + error);			
+			}
+			else{
+				if (tweets){
+					res.json(tweets);
+				}
+				else{
+					res.json("");
+				}
+			}
+		});
 	console.log(rest);
 	res.json(rest);
 };
