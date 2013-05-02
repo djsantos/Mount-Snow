1607f2243d7ffa79de169b8bc3e7b50b01fe02eb
mbohling <mbohling@student.umass.edu>
2013-05-02 14:49:17 -0300

    Update users.sql

--- users.sql
+++ users.sql
@@ -1,6 +1,1 @@
-CREATE TABLE users(
- 	uid INTEGER PRIMARY KEY,
-	username VARCHAR(30),
-	password VARCHAR(30),
-	email VARCHAR(30),
-);
+CREATE TABLE users(uid INTEGER PRIMARY KEY,username VARCHAR(30),password VARCHAR(30),email VARCHAR(30));
