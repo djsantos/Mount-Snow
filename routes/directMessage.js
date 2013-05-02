d4eada3a60a895ef5f2fc5e40d679043c9c0e78d
ddebened <ddebened@student.umass.edu>
2013-05-01 23:01:29 -0300

    Update directMessage.js

--- routes/directMessage.js
+++ routes/directMessage.js
@@ -1,73 +1,54 @@
-*/
-
 /*
  * GET DirectMessage file
  */
 var messageLib = require('../lib/directMessage');
 var user = require('../lib/user');
 
-var message = null;
+var message;
 var myUID = parseInt(-1,10);
-var 
+var to;
 
-//renders sent view
+// Records all the posts made to the server:
+var posts = [];
 
-exports.sent = function(req,res){
-  myUsername = req.session.message;
-  myUID = req.session.uid;
-  if(myUID ===  parseInt(-1,10)) 
-    res.redirect('/welcome');
-  var sentList = messageLib.sentMessages(myUID);
-  res.render('messages', {
-	title: 'Twitter',
-	username: user.getUserName(myUID),
-	messages: sentList
-  });
+// Represents a post:
+function Post(message, myUid, to) {
+	this.message = message;
+	this.myUid = myUid;
+	this.to = to;
+}
+
+//renders the directMessage page
+
+exports.directMessage = function(req,res){
+	var myUID = req.session.uid;
+	var inbox = messageLib.recievedMessages(myUID);
+	if(myUID ===  parseInt(-1,10)) res.redirect('/welcome');
+	res.render('directMessage', {
+		title: 'Twitter',
+		to: user.getUid(to),
+		message: message,
+		inbox: inbox,
+	});
 };
 
-//renders recieved view
 
-exports.recieved = function(req,res){
-  myUsername = req.session.message;
-  myUID = req.session.uid;
-  if(myUID ===  parseInt(-1,10)) 
-  	res.redirect('/welcome');
-  var recievedList = messageLib.recievedMessages(myUID);
-  res.render('messages', {
-	title: 'Twitter',
-	username: user.getUserName(myUID),
-	messages: recievedList
-  });
-};
-
-//renders conversation view
-
-exports.conversation = function(req,res){
-  myUsername = req.session.message;
-  myUID = req.session.uid;
-  if(myUID ===  parseInt(-1,10)) 
-  	res.redirect('/welcome');
-  var conversation = messageLib.displayConversation(myUID, myUID2);
-  res.render('messages', {
-	title: 'Twitter',
-	sender: user.getUserName(myUID),
-	reciever: user.getUserName(myUID2),
-	messages: conversation
-  });
-};
-
-exports.createMessage = function(req, res){
+exports.createMessage = function (req, res) {
 	var message = req.body.message;
 	myUID = req.session.uid;
-	theirUID = user.getUID(req.body.to);
-	Tweetlib.createTweet(message, myUID, theirUID, function(error, id, id2){
-	if(error === 'no message')
-		console.log('enter a message');
-	else if(error === 'invalid id')
-		console.log('invalid id');
-	else if(error === 'invalid id2')
-		console.log('invalid id2');
-	else
-		console.log('message sent');
+	messageLib.createMessage(message, myUID, to, function(error, id){
+		console.log('hello');
+		if(error){
+			console.log(error);
+			res.redirect('/directMessage');
+			return;
+		}
+		else{
+			console.log('received message: ' + message);
+			posts.push(new Post(message,myUid, to));
+			res.json({ status: 'OK'});
+			console.log(posts);
+			res.redirect('/directMessage');
+		}
 	});
 };
