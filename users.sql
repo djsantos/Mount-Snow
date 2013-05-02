70b576d93e019569f27cd9247a35c257ec7f7ab2
mbohling <mbohling@student.umass.edu>
2013-05-02 14:38:26 -0300

    Update users.sql

--- users.sql
+++ users.sql
@@ -1,23 +1,7 @@
-DROP TABLE IF EXISTS users
-DROP TABLE IF EXISTS tweets
-DROP TABLE IF EXISTS follow
-
-
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
  	uid INTEGER PRIMARY KEY autoincrement,
 	username VARCHAR(30) not null,
 	password VARCHAR(30) not null,
 	email VARCHAR(30) not null,
-);
-
-CREATE TABLE tweets (
-	uid INTEGER not null,
-	username TEXT not null,
-	message TEXT not null,
-	time TIMESTAMP not null,
-);
-
-CREATE TABLE follow (
-	uid INTEGER not null,
-	theirID INTEGER not null,
 );
