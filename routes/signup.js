23122413ce38981b999938c896bc3b232b34d1e0
mbohling <mbohling@student.umass.edu>
2013-05-02 15:19:28 -0300

    Update signup.js

--- routes/signup.js
+++ routes/signup.js
@@ -35,10 +35,10 @@ exports.CreateAccount = function (req, res) {
     			var userID = u;
 				req.session.user = username;
 				req.session.uid = userID;
-			db.run("READ users.sql");
+			db.executeSql("READ users.sql");
 			//db.run(".output te")
-              		db.run("INSERT into users values (?, ?, ?, ?)", [userID, username, password, email]);
-              		db.run("SELECT * from users");
+              		//db.run("INSERT into users values (?, ?, ?, ?)", [userID, username, password, email]);
+              		//db.run("SELECT * from users");
               		//, function (error){
                 //if (error){
                   //cb(error);
