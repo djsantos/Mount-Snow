d303a055960e7a3a9ca1451824b0336eb9feaaa4
mbohling <mbohling@student.umass.edu>
2013-05-02 14:50:58 -0300

    Update users.sql

--- users.sql
+++ users.sql
@@ -1,1 +1,6 @@
-CREATE TABLE users(uid INTEGER PRIMARY KEY,username VARCHAR(30),password VARCHAR(30),email VARCHAR(30));
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS tweets;
+DROP TABLE IF EXISTS follow;
+CREATE TABLE users (uid INTEGER PRIMARY KEY autoincrement,username VARCHAR(30) not null,password VARCHAR(30) not null,email VARCHAR(30) not null);
+CREATE TABLE tweets (uid INTEGER not null,username TEXT not null,message TEXT not null,time TIMESTAMP not null);
+CREATE TABLE follow (uid INTEGER not null,theirID INTEGER not null);
