cfea4bf1301529370a7605a6a4b843f797ab93c6
mbohling <mbohling@student.umass.edu>
2013-05-03 14:46:41 -0300

    Update welcome.js

--- routes/welcome.js
+++ routes/welcome.js
@@ -26,7 +26,7 @@ exports.login = function (req, res) {
 			res.redirect ('/signup');
 		}
 		else{
-			user = db.run("select * from UserTable where username = ?;", username);
+			user = db.run("select username from UserTable where username = ?;", username);
 			req.session.user = user;
 			req.session.uid = db.run('SELECT uid from UserTable where username = ?;', username);
 			res.redirect ('/home');
